dabeefdc60741048683f25e7cb762e5616bd226a
Suharjin <suharjin01@gmail.com>
2025-01-23 21:18:15 +0800

    Assertion

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,14 @@ fn main() {
 fn test_simple() {
     println!("Hello Test");
 }
+
+
+fn add(a: i32, b:i32) -> i32 {
+    a + b
+}
+
+#[test]
+fn test_add() {
+    let result = add(5, 6);
+    assert_eq!(result, 11, "5 + 6 Should be 11")
+}
