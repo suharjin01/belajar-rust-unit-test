e4c37ccf0649079f92c810bc565a66770a3d8b40
Suharjin <suharjin01@gmail.com>
2025-01-28 22:12:33 +0800

    Test Module

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,3 +1,16 @@
 pub fn say_hello(name: &str) -> String {
     format!("Hello {}!", name)
 }
+
+
+// Test Module di Library
+#[cfg(test)]
+mod tests {
+    use super::say_hello;
+
+    #[test]
+    fn test_say_hello() {
+        let hello = say_hello("Suharjin");
+        assert_eq!(hello, "Hello Suharjin!")
+    }
+}
